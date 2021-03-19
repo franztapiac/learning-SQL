fab05efa58e6f404ff619bc9fb74766f648cad42
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2021-03-19 20:30:10 +0100

    Included DELETE, UPDATE, INSERT

--- lessons/sql-code-from-lessons.sql
+++ lessons/sql-code-from-lessons.sql
@@ -3,7 +3,7 @@
 /*##########
 # Contents #
 ##########*/
--- Statements: COUNT, DISTINCT, LIMIT, SELECT
+-- Statements: COUNT, DELETE, DISTINCT, INSERT, LIMIT, SELECT, UPDATE
 
 
 /*############
@@ -16,11 +16,18 @@
 	- SELECT COUNT(Locations) from FilmLocations WHERE Writer = "James Cameron";
 	- SELECT COUNT(*) FROM FilmLocations WHERE ReleaseYear < 1950;
 
+-- DELETE
+	- DELETE FROM Instructor WHERE firstname = 'Hima';
+
 --DISTINCT & WHERE
 	- SELECT DISTINCT Title FROM FilmLocations;
 	- SELECT COUNT(DISTINCT ReleaseYear) FROM FilmLocations WHERE ProductionCompany = "Warner Bros. Pictures";
 	- SELECT DISTINCT Title, ReleaseYear from FilmLocations WHERE ReleaseYear >= 2001;
 	- SELECT COUNT(DISTINCT Distributor) FROM FilmLocations WHERE Actor1 = "Clint Eastwood";
+
+-- INSERT
+	- INSERT INTO Instructor(ins_id, lastname, firstname, city, country) VALUES(4, 'Saha', 'Sandip', 'Edmonton', 'CA');
+	- INSERT INTO Instructor(ins_id, lastname, firstname, city, country) VALUES(5, 'Doe', 'John', 'Sydney', 'AU'), (6, 'Doe', 'Jane', 'Dhaka', 'BD');
 
 --LIMIT & OFFSET
 	- SELECT * FROM FilmLocations LIMIT 25;
@@ -34,4 +41,8 @@
 	- SELECT Title, Director, Writer FROM FilmLocations;
 	- SELECT Title, ReleaseYear, Locations FROM FilmLocations WHERE ReleaseYear>=2001;
 	- SELECT Title, ProductionCompany, Locations, ReleaseYear FROM FilmLocations WHERE Writer<>"James Cameron";
+	
+--UPDATE
+	- UPDATE Instructor SET city='Toronto' WHERE firstname="Sandip";
+	- UPDATE Instructor SET city = 'Dubai', country = 'AE' WHERE ins_id = 5;
 	
