bb08eb5b795910279ae56a9175f937060f369cab
Franz Tapia Chaca <franz.tapia-chaca@outlook.com>
2021-03-19 20:02:56 +0100

    Update sql-code-from-lessons.sql
    Fixed SQL code with proper syntax for comments

--- lessons/sql-code-from-lessons.sql
+++ lessons/sql-code-from-lessons.sql
@@ -1,35 +1,35 @@
-This file contains examples of the SQL code that I have learned and applied in the Coursera's course "Databases and SQL for Data Science with Python".
+ /* This file contains examples of the SQL code that I have learned and applied in the Coursera's course "Databases and SQL for Data Science with Python". */
 
-###########
-# Content #
-###########
-- Statements: COUNT, DISTINCT, LIMIT, SELECT
+/*##########
+# Contents #
+##########*/
+-- Statements: COUNT, DISTINCT, LIMIT, SELECT
 
 
-##############
+/*############
 # Statements #
-##############
-(Using SanFranciscoFilmLocations Database):
+############*/
+--(Using SanFranciscoFilmLocations Database):
 
-COUNT & WHERE
+--COUNT & WHERE
 	- SELECT COUNT(*) FROM FilmLocations;
 	- SELECT COUNT(Locations) from FilmLocations WHERE Writer = "James Cameron";
 	- SELECT COUNT(*) FROM FilmLocations WHERE ReleaseYear < 1950;
 
-DISTINCT & WHERE
+--DISTINCT & WHERE
 	- SELECT DISTINCT Title FROM FilmLocations;
 	- SELECT COUNT(DISTINCT ReleaseYear) FROM FilmLocations WHERE ProductionCompany = "Warner Bros. Pictures";
 	- SELECT DISTINCT Title, ReleaseYear from FilmLocations WHERE ReleaseYear >= 2001;
 	- SELECT COUNT(DISTINCT Distributor) FROM FilmLocations WHERE Actor1 = "Clint Eastwood";
 
-LIMIT & OFFSET
+--LIMIT & OFFSET
 	- SELECT * FROM FilmLocations LIMIT 25;
 	- SELECT * FROM FilmLocations LIMIT 15 OFFSET 10;
 	- SELECT DISTINCT Title FROM FilmLocations LIMIT 50;
 	- SELECT DISTINCT Title FROM FilmLocations WHERE ReleaseYear = 2015 LIMIT 10;
 	- SELECT DISTINCT Title FROM FilmLocations WHERE ReleaseYear = 2015 LIMIT 3 OFFSET 5;
 
-SELECT & WHERE 
+--SELECT & WHERE 
 	- SELECT * FROM FilmLocations;
 	- SELECT Title, Director, Writer FROM FilmLocations;
 	- SELECT Title, ReleaseYear, Locations FROM FilmLocations WHERE ReleaseYear>=2001;
